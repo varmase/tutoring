555d74da163f626dd915b34572365db5e387128d
Akhil Kottapalli <akhilk@mac.lan>
2026-02-08 11:07:05 -0800

    -learnt recursions

---
+++ mod2/power.py
@@ -0,0 +1,10 @@
+#def rec(): looping function
+#rec(): how to call def rec():
+#recursive v. non-recursive: recursive calls function inside itself
+
+def power(base, exponent):
+    power(2,3)
+    if exponent != 0:
+        base *  
+        exponent -= 1
+        return(base) 
--- review1/letter_freq.py
+++ review1/letter_freq.py
@@ -1,6 +1,18 @@
+#index: a number that selects individual part of a list or string (letter)
+#Git notes:
+#git pull: updates local (e.g desktop) code
+#git status: Shows branch, changes made
+#git push: updates remote (repository) code
+#git add: command that adds code to repository; part of gitpush
+#git commit: what stores code, message to indicate changes
 def letter_freq():
-    letter = input("")
-    string = input("")
-    lfd = {letter, string}
-    
-
+    letter = input("Enter letter to test it's frequency in a word(s): ")
+    string = input("Enter word(s):")
+    lfd = {}
+    lfd[letter] = 0
+    for ltr in range(len(string)):
+        string[ltr]
+        if string[ltr] == letter:
+            lfd[string[ltr]] += 1
+    return(lfd[letter])
+print("Letter frequency: " + str(letter_freq()))
